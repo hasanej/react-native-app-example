24d5ef00dca3f08315a81887e95f107b77fa0d21
Hasan El Jabir <eljabirhasan@gmail.com>
2019-06-13 13:21:08 +0700

    Add React Redux

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,4 @@
+export constant ADD_PLACE = 'ADD_PLACE';
+export constant DELETE_PLACE = 'DELETE_PLACE';
+export constant SELECT_PLACE= 'SELECT_PLACE';
+export constant DESELECT_PLACE = 'DESELECT_PLACE';
