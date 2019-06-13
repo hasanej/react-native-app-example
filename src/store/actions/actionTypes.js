3c4b889487c1052a4414e9099b4eb523cb1de470
Hasan El Jabir <eljabirhasan@gmail.com>
2019-06-13 14:40:07 +0700

    Connect redux to react native

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,4 +1,4 @@
-export constant ADD_PLACE = 'ADD_PLACE';
-export constant DELETE_PLACE = 'DELETE_PLACE';
-export constant SELECT_PLACE= 'SELECT_PLACE';
-export constant DESELECT_PLACE = 'DESELECT_PLACE';
+export const ADD_PLACE = 'ADD_PLACE';
+export const DELETE_PLACE = 'DELETE_PLACE';
+export const SELECT_PLACE= 'SELECT_PLACE';
+export const DESELECT_PLACE = 'DESELECT_PLACE';
